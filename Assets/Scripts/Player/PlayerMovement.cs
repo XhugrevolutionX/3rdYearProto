6d9648014d6f06a43763b1b54113e67620ac41e2
Jemo <103411555+Jemow@users.noreply.github.com>
2026-05-11 22:26:17 +0200

    chore: imported cinemachine package

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Parameters")]
-    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
     
     private Rigidbody _rb;
     
@@ -22,8 +23,22 @@ public class PlayerMovement : MonoBehaviour
 
     private void Move()
     {
-        _rb.linearVelocity = new Vector3(_direction.x * _speed, _rb.linearVelocity.y, _direction.y * _speed);
+        _rb.linearVelocity = new Vector3(_direction.x * speed, _rb.linearVelocity.y, _direction.y * speed);
+    }
+    
+    private void Jump()
+    {
+        _rb.AddForce(Vector3.up * jumpForce, ForceMode.Force);
     }
 
+    #region Input
+
     public void OnMove(InputAction.CallbackContext context) => _direction = context.ReadValue<Vector2>();
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if(context.started) Jump();
+    }
+
+    #endregion
 }
