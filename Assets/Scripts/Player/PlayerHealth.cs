d361708900e08e775815ca788cdbe0718a41259d
Jemo <103411555+Jemow@users.noreply.github.com>
2026-06-05 17:47:01 +0200

    feat: added camera shake when player is taking damages

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class PlayerHealth : Health
 {
+    [Header("Camera Shake")]
+    [SerializeField] private CameraShakeData hitShakeData;
+    
+    protected override void TakeDamage(int amount)
+    {
+        base.TakeDamage(amount);
+        CameraShakeManager.Instance?.Shake(hitShakeData);
+    }
+
     protected override void Death()
     {
         Debug.Log("Player is death");
