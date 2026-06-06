d7312dced588bbcc9868de112e54167e8a43eea0
Jemo <103411555+Jemow@users.noreply.github.com>
2026-06-06 12:57:30 +0200

    feat: tweak slash color

--- Assets/Scripts/Display/PlayerDisplay.cs
+++ Assets/Scripts/Display/PlayerDisplay.cs
@@ -8,8 +8,8 @@ public class PlayerDisplay : TypeDisplay
     protected override void ApplyColor(Color color)
     {
         base.ApplyColor(color);
-        Color.RGBToHSV(color, out float h, out _, out float v);
-        Color particleColor = Color.HSVToRGB(h, 0.5f, v);
+        Color.RGBToHSV(color, out float h, out float s, out float v);
+        Color particleColor = s == 0f ? color : Color.HSVToRGB(h, 0.8f, v);
 
         foreach (var particle in particles)
         {
